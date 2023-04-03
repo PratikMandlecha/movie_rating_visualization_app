fc93c7c97e0c9c0f8e4fecf26da74903f22cd2c9
Pratik Mandlecha <pmandlecha198@gmail.com>
2023-04-03 16:02:29 -0400

    added code for the movie app

---
+++ README.md
@@ -0,0 +1,18 @@
+# Streamlit Altair app for Movie Rating and recommendation visualization
+
+* **Team members**: pmandlec@andrew.cmu.edu
+* **Online URL**: https://cmu-ids-spring-2023-assignment-3-viz-wizar-streamlit-app-7gtz01.streamlit.app/#qn-give-me-the-names-and-working-hours-of-restaurants-in-philadelphia-located-at-postal-code-19107-with-average-rating-greater-than-3-2
+## Instructions
+
+### Run Locally
+
+Check out the Streamlit [getting started](https://docs.streamlit.io/en/stable/getting_started.html) guide and setup your Python environment.
+
+To run the application locally, install the dependencies with `pip install -r requirements.txt` (or another preferred method to install the dependencies listed in `requirements.txt`). Then run `streamlit run streamlit_app.py`.
+
+### Deploy to Streamlit Sharing
+
+Before you can view your application online, you need to have it set up with Streamlit Cloud. 
+Sign up for a [free Starter Streamlit Cloud account](https://streamlit.io/cloud). 
+
+Then, go to [share.streamlit.io](https://share.streamlit.io) to deploy your Streamlit app by creating a new app and pointing it to your github repo.
---
+++ streamlit_movie.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+
+# Load data from CSV to a pandas dataframe
+
+def load_data():
+    return pd.read_csv("movie_ratings.csv")
+
+# Page title
+st.title("Monitoring the Movie Ratings and Recommendation System")
+
+# Load data
+df = load_data()
+
+# Define color range
+range_colors = ['steelblue', 'lightgray', '#aec7e8', '#ffbb78', '#2ca02c']
+
+# Create a color encoding for ratings
+rating_color = alt.Color('ratings', scale=alt.Scale(domain=['1', '2', '3', '4', '5'], range=range_colors))
+
+# Create a bar chart of movie ratings
+rating_counts = df['ratings'].value_counts().sort_index() 
+chart = alt.Chart(rating_counts.reset_index()).mark_bar().encode(
+    x=alt.Y('index:O', axis=alt.Axis(title='Rating'), sort='-x'),
+    y=alt.X('ratings:Q', axis=alt.Axis(title='Number of Ratings')),
+)
+st.write("Movie rating distribution:")
+st.altair_chart(chart, use_container_width=True)
+
+
+# Create a scatter plot of movie ratings over time
+df['timestamp'] = pd.to_datetime(df['timestamp'])
+chart = alt.Chart(df).mark_circle(size=60).encode(
+    x=alt.X('timestamp', axis=alt.Axis(title='Timestamp')),
+    y=alt.Y('ratings', axis=alt.Axis(title='Rating')),
+    tooltip=['movie_id'],
+    color=rating_color
+).interactive()
+st.write("Movie rating over time:")
+st.altair_chart(chart, use_container_width=True)
+
+
+
+# Display the data in a table
+st.write("Movie ratings data:")
+st.write(df)
+
+st.markdown("<h3 style='color: #FF00FF; font-weight: bold;'>Get details of a random movie rating</h3>", unsafe_allow_html=True)
+if st.button("Get random rating"):
+    mini_df = df.head(1000)
+    movie = mini_df.sample(n=1).iloc[0]
+    st.write(f"""The movie {movie['movie_id']} has a rating of {movie['ratings']} given by user {movie['user_id']} at {movie['timestamp']}""")
